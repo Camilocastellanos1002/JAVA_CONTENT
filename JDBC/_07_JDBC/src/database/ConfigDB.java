187df1810af2f42fce16712fc87df10142e765bb
Juan Camilo <jccaste1002@gmail.com>
2024-03-18 08:12:27 -0500

    creando carpetas

---
+++ JDBC/_07_JDBC/src/database/ConfigDB.java
@@ -0,0 +1,9 @@
+package database;
+
+import java.sql.Connection;
+
+public class ConfigDB {
+    //Variable que va a contener el estado de la conexión
+
+    static Connection objConnection = null;
+}
