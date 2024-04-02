fc8c4e55a2b87d35c20c8882b781707d712db795
Juan Camilo <jccaste1002@gmail.com>
2024-04-02 09:28:57 -0500

    insertando .sql

---
+++ Simulacro/Tablas_Hospital.sql
@@ -0,0 +1,44 @@
+-- CREANDO TABLAS --
+CREATE TABLE especialidad(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR (30) NOT NULL,
+    descripcion VARCHAR (40)
+);
+
+CREATE TABLE medico(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR (15) NOT NULL,
+    apellidos VARCHAR (20) NOT NULL,
+    id_especialidad INT (10),
+    CONSTRAINT fk_id_especialidad FOREIGN KEY (id_especialidad) REFERENCES especialidad (id) ON DELETE CASCADE
+);
+CREATE TABLE paciente(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR (15) NOT NULL,
+    apellidos VARCHAR (20) NOT NULL,
+    fecha_nacimiento DATE,
+    documento_identidad VARCHAR (10) NOT NULL
+);
+CREATE TABLE cita(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    id_paciente INT (10),
+    CONSTRAINT fk_id_paciente FOREIGN KEY (id_paciente) REFERENCES paciente (id) ON DELETE CASCADE,
+    id_medico INT (10),
+    CONSTRAINT fk_id_medico FOREIGN KEY (id_medico) REFERENCES medico (id) ON DELETE CASCADE,
+    fecha_cita DATE NOT NULL,
+    hora_cita TIME NOT NULL,
+    motivo VARCHAR (100) NOT NULL
+);
+
+#insercion de datos en tablas
+INSERT INTO especialidad (nombre,descripcion) VALUES ("Ortopedista", "especialista en arreglo de rodillas"), ("Oncologo", "infantil");
+SELECT * FROM especialidad;
+
+INSERT INTO medico (nombre,apellidos) VALUES ("Cristian","Manco"),("Manuela","Restrepo"),("Luisa","Cardenas");
+SELECT * FROM medico;
+
+INSERT INTO paciente (nombre,apellidos,fecha_nacimiento,documento_identidad) 
+VALUES ("Juan Camilo","Chaparro Castellanos","1996-02-10","1039466438"),("Luciana","Montoya","2010-09-28","1020405020");
+SELECT * FROM paciente; 
+
+
