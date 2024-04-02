d29259277b61719baa846f4b76fd5c817024439f
Juan Camilo <jccaste1002@gmail.com>
2024-04-01 21:54:16 -0500

    actualizacion de entitys

---
+++ Simulacro/Hospital_JDBC/src/entity/Cita.java
@@ -0,0 +1,12 @@
+package entity;
+
+import java.sql.Time;
+import java.util.Date;
+
+public class Cita {
+    private int id;
+    private Date fecha_cita;
+    private Time hora_cita;
+    private String motivo;
+
+}
--- Simulacro/Hospital_JDBC/src/entity/Medico.java
+++ Simulacro/Hospital_JDBC/src/entity/Medico.java
@@ -1,5 +1,8 @@
 package entity;
 
 public class Medico {
+    private int id;
+    private String nombre;
+    private String apellidos;
 
 }
